a83ae3429d6241b9a9a7ddb2c9f2e340534b72a2
Alex Miller <ajm9594@gmail.com>
2021-07-14 13:34:46 -0700

    mobile header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react"
-
+import drawer from '../images/drawer.svg'
+import logoMobile from '../images/DCJam-megaphone-white.png'
 const IndexPage = () => {
   return (
-    <div className="root bg-background-black">
-    <h1 className="font-display text-white">DC-Jam</h1>
-      <p className="font-bold font-body text-highlight-red">Hello World</p>
+    <div className="root min-h-screen bg-background-black">
+      <header className="flex justify-around items-center flex-row py-5">
+        <img src={drawer} alt="" className="drawer-icon"/>
+        <h1 className="font-display text-white text-4xl">DC-Jam<img src={logoMobile} alt="" className="logo-mobile inline mx-2 pb-1"/>Records</h1>
+      </header>
     </div>
   )
 }
