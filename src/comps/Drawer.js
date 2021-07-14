ea298972c52cf93d5b2c271ff133d18c11865e71
Alex Miller <ajm9594@gmail.com>
2021-07-14 14:11:14 -0700

    added side drawer

---
+++ src/comps/Drawer.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const Drawer = () => {
+    return(
+        <nav id="drawer" className="h-screen w-1/2 bg-highlight-red absolute p-2 top-0 -left-full">
+            <h2 className="font-display text-3xl text-white">About</h2>
+            <h2 className="font-display text-3xl text-white">Artists</h2>
+            <h2 className="font-display text-3xl text-white">Catalog</h2>
+            <h2 className="font-display text-3xl text-white">Contact</h2>
+            <h3 className="font-body text-white">Social Links etc</h3>
+        </nav>
+    )
+}
+export default Drawer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,29 @@
 import * as React from "react"
 import drawer from '../images/drawer.svg'
 import logoMobile from '../images/DCJam-megaphone-white.png'
+import Drawer from '../comps/Drawer'
 const IndexPage = () => {
+  const [open, setOpen] = React.useState(false)
+  const expand = () => {
+    const drawer = document.getElementById('drawer')
+    drawer.classList.remove('-left-full')
+    drawer.classList.add('left-0')
+    setOpen(true)
+}
+  const collapse = (target) => {
+    if (open) {
+      const drawer = document.getElementById('drawer')
+      if (target === drawer) {return}
+      drawer.classList.add('-left-full')
+      drawer.classList.remove('left-0')
+      setOpen(false)
+    }
+}
   return (
-    <div className="root min-h-screen bg-background-black">
+    <div className="root min-h-screen bg-background-black" onClick={(e) => collapse(e.target)}>
+      <Drawer/>
       <header className="flex justify-around items-center flex-row py-5">
-        <img src={drawer} alt="" className="drawer-icon"/>
+        <img src={drawer} alt="" className="drawer-icon" onClick={() => expand()}/>
         <h1 className="font-display text-white text-4xl">DC-Jam<img src={logoMobile} alt="" className="logo-mobile inline mx-2 pb-1"/>Records</h1>
       </header>
     </div>
