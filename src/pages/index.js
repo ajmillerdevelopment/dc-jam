fdb2acc354d9deb6eeb11becfbc113bfbed087e9
Alex Miller <ajm9594@gmail.com>
2021-07-13 18:48:58 -0700

    google fonts import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import * as React from "react"
 const IndexPage = () => {
   return (
     <div className="root bg-background-black">
-      <p className="font-bold text-highlight-red">Hello World</p>
+    <h1 className="font-display text-white">DC-Jam</h1>
+      <p className="font-bold font-body text-highlight-red">Hello World</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ['Righteous', 'Arial', 'sans-serif'],
+      'body': ['Oswald', 'Arial', 'sans-serif']
+    },
     extend: {
       colors: {
         'background-black': '#0E0E0E',
