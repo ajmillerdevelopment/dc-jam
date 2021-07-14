ee5e863a6d171f67d0e1a3fad757f5c44e6b2559
Alex Miller <ajm9594@gmail.com>
2021-07-14 16:50:42 -0700

    moved mobile header to sep component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import * as React from "react"
-import drawer from '../images/drawer.svg'
-import logoMobile from '../images/DCJam-megaphone-white.png'
 import Drawer from '../comps/Drawer'
 import sovmech from '../images/sovmach.webp'
 import {Carousel} from "react-responsive-carousel"
@@ -8,6 +6,7 @@ import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import goals from '../images/goals.webp'
 import porcupine from '../images/porcupine.webp'
 import sovmechlp from '../images/sovmechlp.webp'
+import MobileHeader from '../comps/MobileHeader'
 const IndexPage = () => {
   const [open, setOpen] = React.useState(false)
   const expand = () => {
@@ -28,10 +27,7 @@ const IndexPage = () => {
   return (
     <div className="root min-h-screen bg-background-black" onClick={(e) => collapse(e.target)}>
       <Drawer/>
-      <header className="flex justify-around items-center flex-row py-5">
-        <img src={drawer} alt="" className="drawer-icon" onClick={() => expand()}/>
-        <h1 className="font-display text-white text-4xl">DC-Jam<img src={logoMobile} alt="" className="logo-mobile inline mx-2 pb-1"/>Records</h1>
-      </header>
+      <MobileHeader expand={expand}/>
       <article className="relative" >
         <img src={sovmech} alt="" className="w-full"/>
         <h2 className="font-display absolute bottom-0 z-10 text-center w-full txt-shadow text-4xl text-white">DC-Jam Signs the Soviet Machines</h2>
