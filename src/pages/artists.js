d52037d961255dcf0e2d2a0c5e18015574e2a2e5
Alex Miller <ajm9594@gmail.com>
2021-07-23 12:11:36 -0700

    deskop artists

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -4,6 +4,7 @@ import MobileHeader from '../comps/MobileHeader'
 import fishbone from '../images/fishbone.jpg'
 import tsol from '../images/tsol.jpg'
 import richie from '../images/richie.jpg'
+import DesktopHeader from '../comps/DesktopHeader'
 const ArtistsPage = () => {
     const [open, setOpen] = React.useState(false)
     const expand = () => {
@@ -22,24 +23,27 @@ const ArtistsPage = () => {
       }
   }
     return(
-        <div className="root min-h-screen bg-background-black" onClick={(e) => collapse(e.target)}>
+        <div className="root sm:blurred-bg min-h-screen bg-background-black" onClick={(e) => collapse(e.target)}>
             <Drawer/>
             <MobileHeader expand={expand}/>
-            <h2 className="font-display text-white mb-4 text-center text-3xl">Artists</h2>
-            <article className="relative" >
+            <DesktopHeader/>
+            <h2 className="font-display text-white mb-4 text-center txt-shadow text-3xl">Artists</h2>
+            <div className="flex flex-col justify-center sm:flex-row sm:flex-wrap">
+            <article className="relative shadow-lg sm:m-4 sm:max-w-md" >
               <a href="/fishbone">
-              <img src={fishbone} alt="" className="w-full"/>
+              <img src={fishbone} alt="" className="w-full sm:rounded"/>
               <h2 className="font-display absolute bottom-0 z-10 text-center w-full txt-shadow text-3xl text-white">Fishbone</h2>
               </a>
             </article>
-            <article className="relative" >
-              <img src={tsol} alt="" className="w-full"/>
+            <article className="relative shadow-lg sm:m-4 sm:max-w-md" >
+              <img src={tsol} alt="" className="w-full rounded"/>
               <h2 className="font-display absolute bottom-0 z-10 text-center w-full txt-shadow text-3xl text-white">T.S.O.L.</h2>
             </article>
-            <article className="relative" >
-              <img src={richie} alt="" className="w-full"/>
+            <article className="relative shadow-lg sm:m-4 sm:max-w-md" >
+              <img src={richie} alt="" className="w-full rounded"/>
               <h2 className="font-display absolute bottom-0 z-10 text-center w-full txt-shadow text-3xl text-white">Richie Ramone</h2>
             </article>
+            </div>
         </div>
     )
 }
