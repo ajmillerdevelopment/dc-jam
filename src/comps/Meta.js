5fa108046519a8551c43a649fac89ca7970026d6
Alex Miller <ajm9594@gmail.com>
2021-08-12 12:05:46 -0700

    added favicon

--- src/comps/Meta.js
+++ src/comps/Meta.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import favicon from '../images/favicon.ico'
 const Meta = (scroll) => {
     return(
         <Helmet htmlAttributes={{lang: 'en'}}>
             <title>DC-Jam Records</title>
             <meta name="description" content="DC-Jam Records is an American independent record label founded in 2006 by President Darron Hemann, that focuses primarily on the genres of Punk, Ska, and Experimental music." />
+            <link rel="icon" href={favicon} />
         </Helmet>
     )
 }
