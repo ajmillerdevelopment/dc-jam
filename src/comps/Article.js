f6cccc80776cfac71b2d8885b5372325fe34af98
Alex Miller <ajm9594@gmail.com>
2021-08-22 14:20:42 -0700

    index page tweaks

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -2,11 +2,11 @@ import React from 'react'
 const Article = (props) => {
     if (props.containsImage) {
         return(
-            <article className="flex flex-col items-center m-4 ">
-            <div className="relative shadow-lg sm:m-4 sm:max-w-md" >
+            <article className="flex flex-col items-center m-4"> 
+            <div className="relative h-full sm:p-4 sm:py-10 rounded sm:my-container shadow-lg sm:m-4 sm:w-md" >
             <a href={props.slug}>
-            <img src={props.image} alt={props.altText} className="w-full sm:rounded"/>
-            <h3 className="font-display absolute top-2 z-10 text-center w-full txt-shadow text-3xl text-white">{props.title}</h3>
+            <h3 className="font-display mb-1 top-2 z-10 text-center w-full txt-shadow text-3xl text-white">{props.title}</h3>
+            <img src={props.image} alt={props.altText} className="w-screen sm:w-full sm:rounded"/>
             </a>
             </div>
             <div dangerouslySetInnerHTML={{__html: props.content}} className="sm:my-container wp-content h-full shadow-lg"/>
@@ -15,7 +15,7 @@ const Article = (props) => {
     } else {
            return(
         <article className="flex flex-col items-center m-4"> 
-        <div className="relative h-full sm:p-4 sm:py-10 my-10 rounded sm:my-container shadow-lg sm:m-4 sm:w-md" >
+        <div className="relative h-full sm:p-4 sm:py-10 rounded sm:my-container shadow-lg sm:m-4 sm:w-md" >
         <a href={props.slug}>
         <h3 className="font-display top-2 z-10 text-center w-full txt-shadow text-3xl text-white">{props.title}</h3>
         </a>
