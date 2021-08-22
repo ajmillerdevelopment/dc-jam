d8140d9fcbd358aef057cc62b5cc88239eb3d9ea
Alex Miller <ajm9594@gmail.com>
2021-08-22 14:38:20 -0700

    tweaked index page spacing

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -2,14 +2,12 @@ import React from 'react'
 const Article = (props) => {
     if (props.containsImage) {
         return(
-            <article className="flex flex-col items-center m-4"> 
-            <div className="relative h-full sm:p-4 sm:py-10 rounded sm:my-container shadow-lg sm:m-4 sm:w-md" >
+            <article className="flex flex-col items-center m-4 sm:p-5 rounded sm:my-container shadow-lg sm:m-4 sm:w-2/6"> 
             <a href={props.slug}>
             <h3 className="font-display mb-1 top-2 z-10 text-center w-full txt-shadow text-3xl text-white">{props.title}</h3>
-            <img src={props.image} alt={props.altText} className="w-screen sm:w-full sm:rounded"/>
             </a>
-            </div>
-            <div dangerouslySetInnerHTML={{__html: props.content}} className="sm:my-container wp-content h-full shadow-lg"/>
+            <img src={props.image} alt={props.altText} className="max-h-custom sm:rounded"/>
+            <div dangerouslySetInnerHTML={{__html: props.content}} className="wp-content my-2 h-full"/>
             </article>
         )
     } else {
@@ -20,7 +18,7 @@ const Article = (props) => {
         <h3 className="font-display top-2 z-10 text-center w-full txt-shadow text-3xl text-white">{props.title}</h3>
         </a>
         </div>
-        <div dangerouslySetInnerHTML={{__html: props.content}} className="sm:my-container wp-content h-full shadow-lg"/>
+        <div dangerouslySetInnerHTML={{__html: props.content}} className="sm:my-container my-2 wp-content h-full shadow-lg"/>
         </article>
 
     ) 
