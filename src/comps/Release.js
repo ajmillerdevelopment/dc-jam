c5090f9fc5eaa956ba13250f442a59eb7f0f8be0
Alex Miller <ajm9594@gmail.com>
2021-08-31 14:06:04 -0700

    fixed image sizing to be consistent

--- src/comps/Release.js
+++ src/comps/Release.js
@@ -1,9 +1,9 @@
 import React from 'react'
 const Release = (props) => {
     return(            
-    <article className="relative rounded sm:m-4 sm:max-w-md" >
+    <article className="relative tile-small rounded sm:m-4" >
     <a href={props.slug}>
-      <img src={props.image} alt={props.alt} className="w-full shadow-lg sm:rounded"/>
+      <img src={props.image} alt={props.alt} className="shadow-lg sm:rounded"/>
       {/* <div className="absolute bottom-0 w-full z-10">
         <h2 className="font-display text-center w-full txt-shadow text-2xl text-white">{props.title}</h2>
       </div> */}
