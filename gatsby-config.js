239d4b7871c190faed41bb56fad19c9485df1a71
Alex Miller <ajm9594@gmail.com>
2023-10-13 09:27:22 -0700

    update config js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
-  siteMetadata: {
-    title: "dc-jam",
-  },
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    }, 
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: `https://dcjamrecords.com/blog/?graphql`,
-        debug: {
-          graphql: {
-            showQueryOnError: true
-          }
-        }
-      }
-    }
-  ],
+    siteMetadata: {
+        title: "dc-jam",
+    },
+    plugins: [
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-netlify",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                url: `https://dcjamrecords.com/blog/?graphql`,
+                debug: {
+                    graphql: {
+                        showQueryOnError: true,
+                    },
+                },
+            },
+        },
+    ],
 };
